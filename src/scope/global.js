1b565c8f35fe6ba4a3adbfea369c154e93232a78
Jose Rodriguez <josenect@hotmail.es>
2021-02-24 21:49:11 -0500

    Scope Global

---
+++ src/scope/global.js
@@ -0,0 +1,18 @@
+
+var  hello = 'Hello'          // var se puede ambas 
+let world = 'Hello World'    // let somo se puede re asignar pero no re declarar 
+world = 'ultima'
+const helloWorld = 'Hello World'   // const como se puede declarar y asignar al mismo timepo
+
+
+const anotherFunctio = () => {
+    console.log(hello)
+    console.log(world)
+    console.log(helloWorld)
+
+
+
+}
+
+anotherFunctio()
+
